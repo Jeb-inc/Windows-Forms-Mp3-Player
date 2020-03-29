44c721194a6f751e13c8f1b128aca7192acf6b47
Chipley <59574696+Chipley@users.noreply.github.com>
2020-03-29 18:55:50 +0100

    gfdgd

---
+++ Mp3 Player/Mp3 Player/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Mp3_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playBtn = new System.Windows.Forms.Button();
+            this.fileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileSelectBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // playBtn
+            // 
+            this.playBtn.Location = new System.Drawing.Point(173, 61);
+            this.playBtn.Name = "playBtn";
+            this.playBtn.Size = new System.Drawing.Size(75, 23);
+            this.playBtn.TabIndex = 0;
+            this.playBtn.Text = "Play";
+            this.playBtn.UseVisualStyleBackColor = true;
+            this.playBtn.Click += new System.EventHandler(this.playBtn_Click);
+            // 
+            // fileName
+            // 
+            this.fileName.Location = new System.Drawing.Point(73, 12);
+            this.fileName.Name = "fileName";
+            this.fileName.Size = new System.Drawing.Size(393, 20);
+            this.fileName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "File Name:";
+            // 
+            // fileSelectBtn
+            // 
+            this.fileSelectBtn.Location = new System.Drawing.Point(472, 10);
+            this.fileSelectBtn.Name = "fileSelectBtn";
+            this.fileSelectBtn.Size = new System.Drawing.Size(33, 23);
+            this.fileSelectBtn.TabIndex = 5;
+            this.fileSelectBtn.Text = "...";
+            this.fileSelectBtn.UseVisualStyleBackColor = true;
+            this.fileSelectBtn.Click += new System.EventHandler(this.fileSelectBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(254, 61);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(75, 23);
+            this.stopBtn.TabIndex = 6;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 268);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.fileSelectBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fileName);
+            this.Controls.Add(this.playBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playBtn;
+        private System.Windows.Forms.TextBox fileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button fileSelectBtn;
+        private System.Windows.Forms.Button stopBtn;
+    }
+}
+
---
+++ Mp3 Player/Mp3 Player/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mp3_Player
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer player;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            player = new SoundPlayer();
+        }
+
+        private void fileSelectBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "WAV|*.wav", Multiselect = false, ValidateNames = true })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    fileName.Text = ofd.FileName;
+                }
+            }
+        }
+
+        private void playBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(fileName.Text))
+            {
+                return;
+            }
+
+            try
+            {
+                player.SoundLocation = fileName.Text;
+                player.Play();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+    }
+}
